0a8a25746a8b044a6d6592b967ccdbf0fc428a86
Citadel-web025 <cgmfamilies@gmail.com>
2025-10-25 12:51:22 +0100

    Delete About.jsx

--- About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function About(){
-  return (
-    <section>
-      <h2>About Us</h2>
-      <p>We are committed to raising leaders who reflect Christ's character in every sphere of life.</p>
-    </section>
-  )
-}
